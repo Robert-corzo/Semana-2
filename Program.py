424cd0016fab39880f31204fa97553b9d0891c6e
Robert-corzo <robert.mg.corzo@gmail.com>
2026-03-30 19:36:40 -0500

    primer commit

---
+++ Program.py
@@ -0,0 +1,14 @@
+edad : int = 18,
+altura : float = 1.8
+nom : str = "Carlos"
+ape = 'chan'
+esVerde = bool = False
+print(f"{nom} {ape} tiene {edad} años y su carro {"es" if esVerde else "no es"} verde")
+
+print(f"su altura es {altura}")
+
+#operador ternario
+""""
+es= edad > 18 ? " mayor edad" : "niño";
+es= "Mayor edad" if edad > 18 else "niño"
+"""
