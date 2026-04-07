a4b1d6fe18c8ea9d195400ae4994285e582fc48b
Robert-corzo <robert.mg.corzo@gmail.com>
2026-04-06 20:20:55 -0500

    Problema 1

---
+++ problema_a.py
@@ -0,0 +1,6 @@
+"""
+Haz un progrma en Pthon que te muestre un menu con la primera pcion
+que te permita crear persona con nombre y DNI y la aguegué a una lista de personas.
+El menu tendrá una segunda opcion donde mostrará todas las personas.
+El menú tendra una tercera opcion que pedirá un DNI  eliminará a la persona con ese DNI. 
+"""
